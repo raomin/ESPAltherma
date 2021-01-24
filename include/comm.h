6f6e8d6f88db149911c8f0e66881d836f1cd7c4e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-01-24 16:21:33 +0100

    Do not send write than our buffer size

--- include/comm.h
+++ include/comm.h
@@ -23,7 +23,7 @@ bool queryRegistry(char regID, char *buffer)
 
   //Sending command to serial
   MySerial.flush(); //Prevent possible pending info on the read
-  MySerial.write(prep);
+  MySerial.write(prep, 4);
   ulong start = millis();
 
   int len = 0;
