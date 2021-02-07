3a16f59e58db450d64f73ad266c4c1b4b9139f01
Andrea Pierangeli <a.pierangeli@apra.it>
2021-02-07 12:07:43 +0100

    Fix env variable name

--- add_secrets.py
+++ add_secrets.py
@@ -18,15 +18,14 @@ wifi_pwd = os.environ['ESPALTHERMA_WIFI_PWD']
 
 mqtt_host = os.environ['ESPALTHERMA_MQTT_SERVER']
 mqtt_port = os.environ['ESPALTHERMA_MQTT_PORT']
-mqtt_user = os.environ['ESPALTHERMA_WIFI_USERNAME']
+mqtt_user = os.environ['ESPALTHERMA_MQTT_USERNAME']
 mqtt_pwd = os.environ['ESPALTHERMA_MQTT_PWD']
-
 
 env.Append(CPPDEFINES=[
   ("ESPALTHERMA_WIFI_SSID", wifi_ssid),
   ("ESPALTHERMA_WIFI_PWD", wifi_pwd),
   ("ESPALTHERMA_MQTT_SERVER", mqtt_host),
   ("ESPALTHERMA_MQTT_PORT", mqtt_port),
-  ("ESPALTHERMA_WIFI_USERNAME", mqtt_user),
+  ("ESPALTHERMA_MQTT_USERNAME", mqtt_user),
   ("ESPALTHERMA_MQTT_PWD", mqtt_pwd)
 ])
